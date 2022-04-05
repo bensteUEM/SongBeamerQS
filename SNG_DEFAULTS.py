9c8a73ccf07cc4b584d3f315843c588d95babb4c
bensteUEM <Benedict.Stein@gmail.com>
2022-04-05 19:10:59 +0200

    Added some default Values

---
+++ SNG_DEFAULTS.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+SngDefaultHeader = {
+    'LangCount': '1',
+    'Editor': 'Benedict\'s Python Script am ' + str(date.today()),
+    'Version': '3'
+}
+SngIllegalHeader = {
+    'TitleFormat': '',
+    'FontSize': '',
+    'Format': '',
+    'TitleFormat': ''
+}
+
+SngTitleNumberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+SngSongBookPrefix = ['EG', 'FJ', "WWDLP"]
